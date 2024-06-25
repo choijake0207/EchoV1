8ba5f816a27de911b71c371db46ff0cc4896a8ea
choijake0207 <jake.ch0712@gmail.com>
2024-06-25 14:13:21 -0700

    mount user router

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require("express")
 const app = express()
 const db = require("./models")
 
+// route mounting 
+const userRouter = require("./routes/User")
+app.use("/user", userRouter)
+
+
 db.sequelize.sync().then(()=> {
     app.listen(3001, ()=> {
         console.log("server is up and running")
