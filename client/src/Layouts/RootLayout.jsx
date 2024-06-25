eaf96b55cb4f30e1181a4e73e4b7ecccd805b812
choijake0207 <jake.ch0712@gmail.com>
2024-06-25 14:34:57 -0700

    create skeletal UI

--- client/src/Layouts/RootLayout.jsx
+++ client/src/Layouts/RootLayout.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import {NavLink, Outlet} from "react-router-dom"
 
 export default function RootLayout() {
+
+
+
   return (
-    <div>RootLayout</div>
+    <div className="root-layout">
+      <header>
+        <h1 className="logo">Echo</h1>
+        <nav className="nav-bar">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/register">Sign Up</NavLink>
+        </nav>
+      </header>
+      <main><Outlet/></main>
+      
+    </div>
   )
 }
