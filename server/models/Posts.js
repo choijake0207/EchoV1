1521804d0b54b254d58a3847e973bb30eeaa800b
choijake0207 <jake.ch0712@gmail.com>
2024-06-26 15:30:44 -0700

    create post model

---
+++ server/models/Posts.js
@@ -0,0 +1,8 @@
+module.exports = (sequelize, DataTypes) => {
+    const Posts = sequelize.define("Posts", {
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    })
+}
