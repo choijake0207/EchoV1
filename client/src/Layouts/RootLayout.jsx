d936adf0a82751b9eeeccf18de8dd7959331ab61
choijake0207 <jake.ch0712@gmail.com>
2024-06-25 17:03:05 -0700

    add logout function and button

--- client/src/Layouts/RootLayout.jsx
+++ client/src/Layouts/RootLayout.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {NavLink, Outlet} from "react-router-dom"
+import { authorizeContext } from '../Context/AuthContext'
 
 export default function RootLayout() {
 
 
+  const {authorizeState, setAuthorizeState}= useContext(authorizeContext)
+
+  const logOut = () => {
+    localStorage.removeItem("accessToken")
+    setAuthorizeState({username: "", id: 0, authStatus: false})
+  }
 
   return (
     <div className="root-layout">
       <header>
         <h1 className="logo">Echo</h1>
+        {authorizeState.authStatus ? (<><p>Welcome {authorizeState.username}</p></>) : ( <></>)}
+        
         <nav className="nav-bar">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/login">Login</NavLink>
           <NavLink to="/register">Sign Up</NavLink>
+          <button onClick={logOut}>Log Out</button>
         </nav>
       </header>
       <main><Outlet/></main>
