7414ab9226fe424a0026c6c55aa1c56766a8d9c5
choijake0207 <jake.ch0712@gmail.com>
2024-06-26 15:26:01 -0700

    add pfp and bio attributes to model

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,13 +1,25 @@
 module.exports = (sequelize, DataTypes) => {
     const Users = sequelize.define("Users", {
+
         username: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
         },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
+        },
+        profilePictureUrl: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        biography: {
+            type: DataTypes.STRING,
+            allowNull: true,
         }
+
+
 
     })
     return Users
