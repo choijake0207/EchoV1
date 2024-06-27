fa2a57575d065ec212f8bb496edae3b0203f3636
choijake0207 <jake.ch0712@gmail.com>
2024-06-27 14:32:35 -0700

    add conditional render depending on viewing user auth status

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useAuthorize } from '../Context/AuthContext'
 
 export default function Profile() {
+    const {authorizeState} = useAuthorize()
+    const { username } = useParams()
+    const isMyProfile = authorizeState.username === username
+
+
+    console.log(isMyProfile)
+
   return (
     <div className="profile-page">
         <section className="profile-info">
             <div className="profile-details">
-                <h1 className="profile-username">username</h1>
+                <h1 className="profile-username">{username}</h1>
                 <p className="profile-bio">No bio</p>
                 <p className="profile-date">Joined: date</p>
             </div>
-            <button className="profile-edit-btn"></button>
+            {isMyProfile && <button className="profile-edit-btn">Edit Profile</button>}
+            
         </section>
         <section className="profile-feed"> 
             
