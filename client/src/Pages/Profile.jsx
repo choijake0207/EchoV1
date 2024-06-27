563a34f271396a1937c35f7be6c5695e7a8b98e0
choijake0207 <jake.ch0712@gmail.com>
2024-06-27 15:50:51 -0700

    add useEffect to call get profile request

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import { useAuthorize } from '../Context/AuthContext'
+import { getUserProfile } from '../Api/GET'
 
 export default function Profile() {
     const {authorizeState} = useAuthorize()
     const { username } = useParams()
     const isMyProfile = authorizeState.username === username
 
+    useEffect(() => {
+        const userProfileRequest = async () => {
+            try {
+                const data = await getUserProfile(username)
+                console.log(data)
+
+            } catch (error) {
+                console.log(error.response.data.error)
+            }
+        } 
+        userProfileRequest()
+    }, [username])
 
     console.log(isMyProfile)
 
