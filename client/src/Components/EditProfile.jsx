c54055e22c8d89c332e2065fc396cc6a1b2f00f2
choijake0207 <jake.ch0712@gmail.com>
2024-06-27 17:22:29 -0700

    pass profile info props and functions

--- client/src/Components/EditProfile.jsx
+++ client/src/Components/EditProfile.jsx
@@ -1,20 +1,26 @@
 import React, {useState} from 'react'
 
 
-export default function EditProfile({username, bio}) {
+export default function EditProfile({username, bio, onClose, onSave}) {
 
-    const [newUsername, setNewUsername] = useState("")
-    const [newBio, setNewBio] = useState("")
+    const [newUsername, setNewUsername] = useState(username)
+    const [newBio, setNewBio] = useState(bio)
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      onSave(newUsername, newBio)
+    }
 
 
   return (
     <div className="edit-profile-page">
         <form className="edit-profile-form" id="user-form">
             <label>Username</label>
-            <input type="text"/>
+            <input type="text" value={newUsername} onChange={(e) => setNewUsername(e.target.value)}/>
             <label>Bio</label>
-            <input type="text"/>
-            <button>Save Changes</button>
+            <input type="text" value={newBio} onChange={(e) => setNewBio(e.target.value)}/>
+            <button type="submit">Save Changes</button>
+            <button type="button" onClick={onClose}>Exit</button>
         </form>
     </div>
   )
