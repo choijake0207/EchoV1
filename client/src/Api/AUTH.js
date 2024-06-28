4f856a7b864748f792d790259b6f822f20899caa
choijake0207 <jake.ch0712@gmail.com>
2024-06-28 15:07:38 -0700

    import authorize, login, and register requests to central file

--- client/src/Api/AUTH.js
+++ client/src/Api/AUTH.js
@@ -1,1 +1,26 @@
 import axios from "axios";
+
+export const authorizeUser = async () => {
+    const response = await axios.get("http://localhost:3001/user/auth", {
+        headers: {
+            accessToken: localStorage.getItem("accessToken")
+        }
+    })
+    return response.data
+}
+
+export const login = async (username, password) => {
+    const response = await axios.post("http://localhost:3001/user/login", {
+        username, 
+        password
+    })
+    return response.data
+}
+
+export const register = async (username, password) => {
+    const response = await axios.post("http://localhost:3001/user/register", {
+        username,
+        password
+    })
+    return response.data
+}
