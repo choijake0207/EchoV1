85b420cdedd70845eed507bbafed9e650849a971
choijake0207 <jake.ch0712@gmail.com>
2024-06-28 13:40:36 -0700

    add password form toggling

--- client/src/Pages/Settings.jsx
+++ client/src/Pages/Settings.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ChangePassword from '../Components/ChangePassword'
 
 export default function Settings() {
+
+    const [passwordForm, setPasswordForm] = useState(false)
+    const togglePasswordForm = () => {
+        setPasswordForm(true)
+    }
+
   return (
     <div className="settings-page">
         <header>
             <button type="button">Exit</button>
             <h4>Settings</h4>
         </header>
+        {passwordForm && <ChangePassword/>}
         <section className="account-settings">
             <h3>Account</h3>
             <ul>
                 <li>
-                    <button type="button">Change Password</button>
+                    <button type="button" onClick={togglePasswordForm}>Change Password</button>
                 </li>
             </ul>
         </section>
