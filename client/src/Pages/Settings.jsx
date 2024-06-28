f6be75f8fad6a2aea0508ac40679ba046f3f5043
choijake0207 <jake.ch0712@gmail.com>
2024-06-28 13:15:52 -0700

    create basic ui for settings page

--- client/src/Pages/Settings.jsx
+++ client/src/Pages/Settings.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Settings() {
   return (
-    <div>Settings</div>
+    <div className="settings-page">
+        <header>
+            <button type="button">Exit</button>
+            <h4>Settings</h4>
+        </header>
+        <section className="account-settings">
+            <h3>Account</h3>
+            <ul>
+                <li>
+                    <button type="button">Change Password</button>
+                </li>
+            </ul>
+        </section>
+        <section className="general-settings">
+            <h3>General</h3>
+        </section>
+    </div>
   )
 }
