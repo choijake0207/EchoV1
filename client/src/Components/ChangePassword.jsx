1c77276d5c7d477d4e347160515c2743c07d6e92
choijake0207 <jake.ch0712@gmail.com>
2024-06-28 13:33:42 -0700

    add UI and prop functions

--- client/src/Components/ChangePassword.jsx
+++ client/src/Components/ChangePassword.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function ChangePassword() {
+export default function ChangePassword(onClose, onSave) {
+
+    const [currentPassword, setCurrentPassword] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+
   return (
-    <div>ChangePassword</div>
+    <div className="change-password-page">
+        <header>
+            
+            <h4>Change Password</h4>
+        </header>
+        <form className="change-password-form" id="user-form" onSubmit={handleSubmit}>
+            <label>Current Password</label>
+            <input type="password" onChange={(e)=> setCurrentPassword(e.target.value)} value={currentPassword}/>
+            <label>New Password</label>
+            <input type="password" onChange={(e)=> setNewPassword(e.target.value)} value={newPassword}/>
+            <button type="submit">Save Changes</button>
+            <button type="button" onClick={onClose}>Exit</button>
+        </form>
+    </div>
   )
 }
