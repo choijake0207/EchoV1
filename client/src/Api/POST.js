54ea699b1dbafd528532b0e97c3031a75bcf0467
choijake0207 <jake.ch0712@gmail.com>
2024-07-01 13:04:16 -0700

    add missing headers to request

--- client/src/Api/POST.js
+++ client/src/Api/POST.js
@@ -4,7 +4,11 @@ import axios from "axios";
 
 export const followUser = async (id) => {
     try {
-        const response = await axios.post(`http://localhost:3001/follow/${id}`)
+        const response = await axios.post(`http://localhost:3001/follow/${id}`, {
+            headers: {
+                accessToken: localStorage.getItem("accessToken")
+            }
+        })
         return response.data
     } catch (error) {
         throw error
