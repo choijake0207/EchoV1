c1baba67511357313c2bb8deec69ec1712290f8e
choijake0207 <jake.ch0712@gmail.com>
2024-07-01 12:49:34 -0700

    create follow api call in centralized file

--- client/src/Api/POST.js
+++ client/src/Api/POST.js
@@ -1,3 +1,12 @@
 
 
 import axios from "axios";
+
+export const followUser = async (id) => {
+    try {
+        const response = await axios.post(`http://localhost:3001/follow/${id}`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
