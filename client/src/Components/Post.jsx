452d03471a544a746339519e3e41666e637b9ab7
choijake0207 <jake.ch0712@gmail.com>
2024-07-02 14:20:52 -0700

    add navlink to poster profile and nav function to route to single view page

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 
-export default function Post({username, text, createdAt}) {
+
+export default function Post({id, username, text, createdAt}) {
+  const navigate = useNavigate()
   return (
     <li className="post">
-        <p>{username}</p>
-        <p>{text}</p>
+        <NavLink to={`/profile/${username}`}>{username}</NavLink>
+        <p onClick={() => navigate(`/post/${id}`)}>
+          {text}
+        </p>
         <p>{new Date(createdAt).toLocaleString()}</p>
     </li>
   )
