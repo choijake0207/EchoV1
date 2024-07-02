9348459c2fc172c7adcac087f4403cb13b3833f5
choijake0207 <jake.ch0712@gmail.com>
2024-07-02 11:53:00 -0700

    create comments model and post/user associations

---
+++ server/models/Comments.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+    const Comments = sequelize.define("Comments", {
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    })
+
+    Comments.associate = (models) => {
+        Comments.belongsTo(models.Posts), {
+            foreignKey: "postId",
+            onDelete: "CASCADE"
+        },
+        Comments.belongsTo(models.Users), {
+            foreignKey: "userId",
+            onDelete: "CASCADE"
+        }
+    }
+    return Comments
+}
