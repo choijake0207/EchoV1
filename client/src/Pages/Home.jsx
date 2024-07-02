c8934d27834dcea30394a51659f0a8ba2fde48ae
choijake0207 <jake.ch0712@gmail.com>
2024-07-02 12:48:55 -0700

    create home page ui

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useAuthorize } from '../Context/AuthContext'
+import { NavLink } from 'react-router-dom'
 
 export default function Home() {
+  const {authorizeState} = useAuthorize()
+  const [postText, setPostText] = useState("")
+
+  
+
   return (
-    <div>Home</div>
+    <div className="page" id="home-page">
+      {authorizeState.authStatus ? (
+        <div className="greeting">
+          <h1>Welcome To Echo</h1>
+          <div className="links-container">
+            <NavLink
+              to="/login"
+            >Login</NavLink>
+            <NavLink
+              to="/register"
+            >Register</NavLink>
+          </div>
+        </div>
+      ) : (
+        <form className="create-post-form">
+          <input 
+            type="text" 
+            value={postText}
+            onChange={(e) => setPostText(e.target.value)}
+          ></input>
+          <button type="submit">Post</button>
+        </form>
+      )}
+      <div className="home-feed"></div>
+      
+    </div>
   )
 }
