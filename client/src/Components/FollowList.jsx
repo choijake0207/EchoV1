34fa5bb700dee97fae364220c9566003f390c189
choijake0207 <jake.ch0712@gmail.com>
2024-07-02 11:19:44 -0700

    conditionally render follower/following list through props and add navlinks to users

--- client/src/Components/FollowList.jsx
+++ client/src/Components/FollowList.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-export default function FollowList({list, type}) {
+export default function FollowList({list, type, onClose}) {
   return (
-    <div id="follow-list">FollowList</div>
+    <div className="follow-list">
+        <header>
+            <button onClick={onClose}>Exit</button>
+            <h4>{type === "followers" ? "Followers" : "Following"}</h4>
+        </header>
+        <ul>
+            {list.map(user => (
+                <li key={user.id}>
+                    <NavLink 
+                        to={`/profile/${user.username}`}
+                        onClick={onClose}
+                    >
+                        {user.username}
+                    </NavLink>
+                
+                </li>
+            ))}
+        </ul>
+
+        
+    </div>
   )
 }
