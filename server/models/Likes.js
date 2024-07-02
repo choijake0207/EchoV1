1d7df252d8fd232be11765b8bec160a7bef709bf
choijake0207 <jake.ch0712@gmail.com>
2024-07-02 11:45:49 -0700

    create like model and post/user associations

---
+++ server/models/Likes.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize) => {
+    const Likes = sequelize.define("Likes")
+
+    Likes.associate = (models) => {
+        Likes.belongsTo(models.Posts, {
+            foreignKey: "postId",
+            onDelete: "CASCADE"
+        }),
+        Likes.belongsTo(models.Users, {
+            foreignKey: "userId",
+            onDelete: "CASCADE"
+        })
+    }
+    return Likes
+}
