8a01d7565eb7a53361711543df6f4abc199b607e
choijake0207 <jake.ch0712@gmail.com>
2024-07-03 11:43:39 -0700

    add handleDelete function that calls api

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -2,17 +2,31 @@ import React from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import "../Styles/post.css"
 import { useAuthorize } from '../Context/AuthContext'
+import { deletePost } from '../Api/DELETE'
 
 
 export default function Post({id, username, text, createdAt, userId}) {
   const navigate = useNavigate()
   const {authorizeState} = useAuthorize()
+
+  const handleDelete = async () => {
+    try {
+      await deletePost(id)
+    } catch (error) {
+      console.log(error.response.data.error)
+    }
+  }
+
+
+
   return (
     <li className="post">
         <header>
           <NavLink className="username" to={`/profile/${username}`}>{username}</NavLink>
           {userId === authorizeState.id && (
-            <button>Delete</button>
+            <button
+              onClick={handleDelete}
+            >Delete</button>
           )}
         </header>
         
