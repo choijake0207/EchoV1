ae6b3e335d6c03e7f8173c679b3824a784422ef1
choijake0207 <jake.ch0712@gmail.com>
2024-07-03 11:30:49 -0700

    create deletePost api

--- client/src/Api/DELETE.js
+++ client/src/Api/DELETE.js
@@ -1,1 +1,14 @@
 import axios from "axios";
+
+export const deletePost = async (id) => {
+    try {
+        const response = await axios.delete(`http://localhost:3001/post/${id}`, {}, {
+            headers: {
+                accessToken: localStorage.getItem("accessToken")
+            }
+        })
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
