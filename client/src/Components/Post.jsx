b510105b6928e7f3ff6b9bbd6b6c1e02da9d7ab5
choijake0207 <jake.ch0712@gmail.com>
2024-07-03 11:09:56 -0700

    add userId as a prop

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
+import "../Styles/post.css"
 
 
 export default function Post({id, username, text, createdAt}) {
   const navigate = useNavigate()
   return (
     <li className="post">
-        <NavLink to={`/profile/${username}`}>{username}</NavLink>
-        <p onClick={() => navigate(`/post/${id}`)}>
+        <NavLink className="username" to={`/profile/${username}`}>{username}</NavLink>
+        <p className="text" onClick={() => navigate(`/post/${id}`)}>
           {text}
         </p>
-        <p>{new Date(createdAt).toLocaleString()}</p>
+        <p className="date">{new Date(createdAt).toLocaleString()}</p>
     </li>
   )
 }
