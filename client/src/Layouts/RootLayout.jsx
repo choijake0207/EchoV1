e83b4eb100fd78d22de009c74a3ea4af30b9ee02
choijake0207 <jake.ch0712@gmail.com>
2024-07-03 16:28:58 -0700

    refactor header into side nav bar, refactor conditional nav links, add icons

--- client/src/Layouts/RootLayout.jsx
+++ client/src/Layouts/RootLayout.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect, useRef } from 'react'
 import {NavLink, Outlet, useNavigate} from "react-router-dom"
 import { useAuthorize } from '../Context/AuthContext'
 import "../Styles/root.css"
+import {House, MagnifyingGlass, User, SignOut, Gear, Bell, ChatCircle, UserCircle  } from "phosphor-react"
 
 export default function RootLayout() { 
 
@@ -23,44 +24,41 @@ export default function RootLayout() {
   console.log(authorizeState)
   return (
     <div className="root-layout">
-      <header className="root-header">
-        <div className="nav-wrapper">
-
-        
-          <div className="nav-bar-left">
-            <h1 className="logo">echo</h1>
-            {authorizeState.authStatus ? (<><p>Welcome {authorizeState.username}</p></>) : (null)}
-          </div>
-          <nav className="nav-bar-right">
-            <NavLink to="/">Home</NavLink>
-            {authorizeState.authStatus && 
-              <NavLink to="/search">Search</NavLink>
-            }
-
-            
-            <button onClick={toggleProfileMenu} >Profile</button>
+      <aside className="root-sidebar">
+        <div className="sidebar-top">
+            <h1 className="logo">ECHO</h1>
+        </div>
+        <nav className="sidebar-middle">
+          <NavLink to="/"><House/> Home</NavLink>
+          <NavLink to="/search"><MagnifyingGlass/>Search</NavLink>
+          {authorizeState.authStatus && (
+            <>
+              <NavLink><Bell/>Notifications</NavLink>
+              <NavLink><ChatCircle/>Chat</NavLink>
+              <NavLink to={`/profile/${authorizeState.username}`}><User/>Profile</NavLink>
+            </>
+          )}
+          <NavLink to="/settings"><Gear/>Settings</NavLink>
+        </nav>
+        {authorizeState.authStatus && (
+          <div className="sidebar-bottom">
+            <button onClick={toggleProfileMenu}><UserCircle size={"30px"}/>{authorizeState.username}</button>
             {isProfileMenuOpen && (
-              <div className="profile-menu" onClick={toggleProfileMenu}>
-                {authorizeState.authStatus ? (
-                  <>
-                    <NavLink to={`/profile/${authorizeState.username}`}>View Profile</NavLink>
-                    <NavLink to="/settings">Settings</NavLink>
-                    <button onClick={handleLogOut}>Log Out</button>
-                  </>
-                ) : (
-                  <>
-                    <NavLink to="/register">Sign Up</NavLink>
-                    <NavLink to="/login">Login</NavLink>
-                  </>
-                )}
+              <div className="profile-menu" onClick={toggleProfileMenu}> 
+                <button onClick={handleLogOut}>Log Out</button>
               </div>
             )}
-            
-            
-          </nav>
-        </div>
-      </header>
+          </div>
+       
+        )}
+       
+     
+      </aside>
       <main><Outlet/></main>
+      <aside className="info-sidebar">
+        <div className="info-content"></div>
+
+      </aside>
       
     </div>
   )
