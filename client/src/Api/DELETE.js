38935673a74e9c6c91eecf2de082562f29f57804
choijake0207 <jake.ch0712@gmail.com>
2024-07-03 11:42:30 -0700

    remove empty {} body because delete call only takes 2 arguments

--- client/src/Api/DELETE.js
+++ client/src/Api/DELETE.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+// axios delete function only takes 2 arguments
 export const deletePost = async (id) => {
     try {
-        const response = await axios.delete(`http://localhost:3001/post/${id}`, {}, {
+        const response = await axios.delete(`http://localhost:3001/post/${id}`,  {
             headers: {
                 accessToken: localStorage.getItem("accessToken")
             }
