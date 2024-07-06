da8809df4fe8dbcd38db2f285017c1a357dd1f09
choijake0207 <jake.ch0712@gmail.com>
2024-07-06 13:13:27 -0700

    created generic page component as parent wrapper for all pages except home
    added props for customization by page

---
+++ client/src/Components/GenericPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {ArrowCircleLeft } from 'phosphor-react'
+import { useNavigate } from 'react-router-dom'
+
+export default function GenericPage({children, headerTitle, pageId}) {
+    const navigate = useNavigate()
+  return (
+    <div className="page" id={pageId}>
+        <header className="page-header">
+            <button 
+                type="button"
+                onClick={() => navigate(-1)}
+            >
+                <ArrowCircleLeft size={"30px"}/>
+            </button>
+            <h4>{headerTitle}</h4>
+        </header>
+        <main>{children}</main>
+    </div>
+  )
+}
