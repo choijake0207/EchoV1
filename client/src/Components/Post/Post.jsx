37f4f0784b4cf488771fc4d59ec7f91f0bc7a42c
choijake0207 <jake.ch0712@gmail.com>
2024-10-08 14:44:35 -0400

    clean up and modularize for readability

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -5,6 +5,60 @@ import { useAuthorize } from '../../Context/AuthContext'
 import { deletePost } from '../../Api/DELETE'
 import {HeartStraight, BookmarkSimple, ChatCircle, UserCircle} from "phosphor-react"
 
+
+function PostHeader ({username, auth, handleDelete, userId}) {
+  return (
+    <header>
+      <NavLink className="username" to={`/profile/${username}`}>
+        <UserCircle/>
+        {username}
+      </NavLink>
+      {userId === auth.id && (
+        <button
+          onClick={handleDelete}
+          className="delete-post-btn"
+        >
+          Delete
+        </button>
+      )}
+    </header>
+  )
+}
+
+function PostContent ({id, createdAt, text, navigate}) {
+  return (
+    <div className="post-content">
+      <p className="text" onClick={() => navigate(`/post/${id}`)}>
+        {text}
+      </p>
+      <p className="date">{new Date(createdAt).toLocaleString()}</p>
+    </div>
+  )
+}
+
+function PostDetails ({isHomeView, id, comments, navigate}) {
+  return (
+    <div className="post-details">
+      <div className="post-likes">
+        <button type="button"><HeartStraight/>
+          <p>0</p>
+        </button>
+      </div>
+      <div className="post-comments">
+        <button onClick={isHomeView ? () => navigate(`/post/${id}`) : null}>
+          <ChatCircle />
+          <p>{comments.length}</p>
+        </button>
+      </div>
+      <div className="post-save">
+        <button type="button">
+          <BookmarkSimple size={"20px"}/>
+        </button>
+      </div>
+    </div>
+  )
+}
+
 export default function Post({id, username, text, createdAt, userId, onDelete, comments, isHomeView}) {
   const navigate = useNavigate()
   const {authorizeState} = useAuthorize()
@@ -21,56 +75,26 @@ export default function Post({id, username, text, createdAt, userId, onDelete, c
     }
   }
 
-
-
   return (
     <li className="post">
-        <header>
-          <NavLink className="username" to={`/profile/${username}`}>
-            <UserCircle/>
-            {username}
-          </NavLink>
-          {userId === authorizeState.id && (
-            <button
-              onClick={handleDelete}
-              className="delete-post-btn"
-            >
-              Delete
-            </button>
-          )}
-        </header>
-        <div className="post-content">
-          <p className="text" onClick={() => navigate(`/post/${id}`)}>
-            {text}
-          </p>
-          <p className="date">{new Date(createdAt).toLocaleString()}</p>
-        </div>
-        <div className="post-details">
-            <div className="post-likes">
-              <button type="button"><HeartStraight/>
-                <p>0</p>
-              </button>
-            </div>
-            <div className="post-comments">
-              {isHomeView ? (
-                <button onClick={()=> navigate(`/post/${id}`)}>
-                  <ChatCircle/>
-                  <p>{comments.length}</p>
-                </button>
-              ) : (
-                <div>
-                  <ChatCircle/>
-                  <p>{comments.length}</p>
-                </div>
-              )}
-              
-            </div>
-            <div className="post-save">
-              <button type="button"><BookmarkSimple size={"20px"}/></button>
-            </div>
-            
-        </div>
-
+        <PostHeader 
+          username={username} 
+          auth={authorizeState} 
+          handleDelete={handleDelete} 
+          userId={userId}
+        />
+        <PostContent 
+          id={id} 
+          createdAt={createdAt} 
+          text={text}
+          navigate={navigate}
+        />
+        <PostDetails
+          isHomeView={isHomeView}
+          id={id}
+          comments={comments}
+          navigate={navigate}
+        />
     </li>
   )
 }
