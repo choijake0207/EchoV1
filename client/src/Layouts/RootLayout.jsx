82bbf7659950fdb425a1c29db9d8a49095609a97
choijake0207 <jake.ch0712@gmail.com>
2024-10-08 14:25:32 -0400

    move profile menu into header

--- client/src/Layouts/RootLayout.jsx
+++ client/src/Layouts/RootLayout.jsx
@@ -26,21 +26,6 @@ export default function RootLayout() {
     <div className="root-layout">
       <header className="root-header">
         <h1 className="logo">echo</h1>
-      </header>
-      <aside className="root-nav">
-        <nav className="nav-links">
-          <NavLink to="/"><House/><span className="nav-text">Home</span></NavLink>
-          <NavLink to="/search"><MagnifyingGlass/><span className="nav-text">Search</span></NavLink>
-          {authorizeState.authStatus && (
-            <>
-              <NavLink to="/notifications"><Bell/><span className="nav-text">Notifications</span></NavLink>
-              <NavLink to="/chat"><ChatCircle/>Chat</NavLink>
-              <NavLink to={`/profile/${authorizeState.username}`}><User/><span className="nav-text">Profile</span></NavLink>
-              <NavLink to="/bookmark"><BookmarkSimple/><span className="nav-text">Saved</span></NavLink>
-            </>
-          )}
-          <NavLink to="/settings"><Gear/><span className="nav-text">Home</span></NavLink>
-        </nav>
         {authorizeState.authStatus && (
           <div className="nav-bottom">
             <button onClick={toggleProfileMenu}><UserCircle size={"30px"}/><span className="nav-text">{authorizeState.username}</span></button>
@@ -52,6 +37,21 @@ export default function RootLayout() {
           </div>
        
         )}
+      </header>
+      <aside className="root-nav">
+        <nav className="nav-links">
+          <NavLink to="/"><House/><span className="nav-text">Home</span></NavLink>
+          <NavLink to="/search"><MagnifyingGlass/><span className="nav-text">Search</span></NavLink>
+          {authorizeState.authStatus && (
+            <>
+              <NavLink to="/notifications"><Bell/><span className="nav-text">Notifications</span></NavLink>
+              <NavLink to={`/profile/${authorizeState.username}`}><User/><span className="nav-text">Profile</span></NavLink>
+              <NavLink to="/bookmark"><BookmarkSimple/><span className="nav-text">Saved</span></NavLink>
+            </>
+          )}
+          <NavLink to="/settings"><Gear/><span className="nav-text">Settings</span></NavLink>
+        </nav>
+        
        
      
       </aside>
