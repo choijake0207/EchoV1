4e01235839e5e0ffa4e4abfaa8cd931f63b27aa5
choijake0207 <jake.ch0712@gmail.com>
2024-10-09 14:09:51 -0400

    add: user icons, pop up overlay

--- client/src/Components/PopUps/FollowList.jsx
+++ client/src/Components/PopUps/FollowList.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import UserIcon from '../UserIcon/UserIcon'
 
 export default function FollowList({list, type, onClose}) {
   return (
-    <div className="follow-list">
-        <header>
-            <button onClick={onClose}>Exit</button>
-            <h4>{type === "followers" ? "Followers" : "Following"}</h4>
-        </header>
-        <ul>
-            {list.map(user => (
-                <li key={user.id}>
-                    <NavLink 
-                        to={`/profile/${user.username}`}
-                        onClick={onClose}
-                    >
-                        {user.username}
-                    </NavLink>
-                
-                </li>
-            ))}
-        </ul>
-
-        
+    <div className="follow-list-overlay">
+        <div className="follow-list-container">
+            <header>
+                <h4>{type === "followers" ? `Followers (${list.length})` : `Following (${list.length})`}</h4>
+                <button onClick={onClose}>Exit</button>
+            </header>
+            <ul className="follow-list">
+                {list.map(user => (
+                    <li key={user.id}>
+                        <NavLink 
+                            to={`/profile/${user.username}`}
+                            onClick={onClose}
+                        >
+                            <UserIcon username={user.username}/>
+                            {user.username}
+                        </NavLink>
+                    
+                    </li>
+                ))}
+            </ul>
+        </div>
     </div>
   )
 }
