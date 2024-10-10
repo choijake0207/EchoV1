135e1489e3070ff9bfadba324689358be2e239b7
choijake0207 <jake.ch0712@gmail.com>
2024-10-10 16:35:31 -0400

    add: ref for outside click handling, navlink routing

--- client/src/Components/PopUps/HeaderProfileMenu.jsx
+++ client/src/Components/PopUps/HeaderProfileMenu.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import { SignOut, Info, UserCircle, Gear, Moon, Sun} from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
 import { useDarkMode } from '../../Context/DarkContext'
 
-export default function HeaderProfileMenu({handleLogOut}) {
+
+export default function HeaderProfileMenu({handleLogOut, toggleProfileMenu, username}) {
+    const profileMenuRef = useRef(null)
     const {toggleDarkMode, isDarkMode} = useDarkMode()
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleOutsideClick)
+        return (() => document.removeEventListener("mousedown", handleOutsideClick))
+    },[])
+    const handleOutsideClick = (event) => {
+        if (profileMenuRef.current && !profileMenuRef.current.contains(event.target)) {
+            toggleProfileMenu()
+        }
+    }
   return (
-    <div className="header-profile-menu">
+    <div className="header-profile-menu" ref={profileMenuRef} onClick={toggleProfileMenu} >
         <div className="profile-menu-links">
-            <NavLink><UserCircle/>View Profile</NavLink>
-            <NavLink><Info/>About Echo</NavLink>
-            <NavLink><Gear/>Settings</NavLink>
+            <NavLink to={`/profile/${username}`}><UserCircle/>View Profile</NavLink>
+            <NavLink to="/about"><Info/>About Echo</NavLink>
+            <NavLink to="/settings"><Gear/>Settings</NavLink>
         </div>
         <button onClick={toggleDarkMode}>
             {isDarkMode ? (<><Sun/> Light Mode</>) : (<><Moon/> Dark Mode</>)}
