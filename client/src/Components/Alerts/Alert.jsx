0dac2d4b4ce5e1b626d8bdcaa91168326b6d916f
choijake0207 <jake.ch0712@gmail.com>
2024-10-10 18:50:58 -0400

    refactor alert function

--- client/src/Components/Alerts/Alert.jsx
+++ client/src/Components/Alerts/Alert.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {X} from "phosphor-react"
 
-export default function Alert({message, onClose}) {
+export default function Alert({type, message, onClose}) {
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      document.querySelector(".custom-alert-box").classList.add("alert-hide")
+      setTimeout(() => {
+        onClose()
+      }, 100)
+    }, 3000)
+    return (() => clearTimeout(timer))
+  }, [])
+
+
   return (
-    <div className="custom-alert-box">
-        <div className="custom-alert">
-            <p>{message}</p>
-            <button onClick={onClose}className="alert-close">Close</button>
-        </div>
+    <div className="custom-alert-box" id={`alert-${type}`}>
+        <p>{message}</p>
+        <button onClick={onClose}><X/></button>
     </div>
   )
 }
