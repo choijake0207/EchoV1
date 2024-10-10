756a17e44026753c675494adcf972e053a01057e
choijake0207 <jake.ch0712@gmail.com>
2024-10-10 17:42:43 -0400

    hoist color up to profile page parent

--- client/src/Components/UserIcon/UserIcon.jsx
+++ client/src/Components/UserIcon/UserIcon.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import "./UserIcon.css"
 
 
@@ -7,22 +7,29 @@ function pickIconColor(username) {
   let number = username.length
   switch (number % 5) {
     case 1: 
-      return "rgb(245, 87, 87)"
+      return "var(--default-yellow)"
     case 2:
       return "var(--logo-color)"
     case 3:
-      return "rgb(19, 176, 82)"
+      return "var(--default-green)"
     case 4:
-      return "rgb(230, 53, 198)"
+      return "var(--default-red)"
     default:
       return "grey"
   }
 
 }
 
-export default function UserIcon({username}) {
-    const letter = username.substring(0,1)
+export default function UserIcon({username, handleColor}) {
+    const letter = username.substring(0,1).toUpperCase()
     const color = pickIconColor(username)
+    
+    useEffect(() => {
+      if (handleColor) {
+      handleColor(color)
+      }
+    }, [color])
+    
  
   return (
     <div className="user-icon" style={{backgroundColor: color}}>
