24b42a260645ee33c2f22ac70986e106e4a79e09
choijake0207 <jake.ch0712@gmail.com>
2024-10-11 00:58:50 -0400

    add: news api call, memoize 24 hour period news, render

--- client/src/Components/Sidebar/InfoContent.jsx
+++ client/src/Components/Sidebar/InfoContent.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { memo, useEffect, useState } from 'react'
+import {TrendUp} from "phosphor-react"
+import { fetchNews } from '../../Api/GET'
+
+
 
 export default function InfoContent() {
+
+  const [news, setNews] = useState([])
+
+  useEffect(() => {
+    const handleFetchNews = async () => {
+
+      const memoizedNews = localStorage.getItem("memoizedNews")
+      const memoizedTime = localStorage.getItem("memoizedTime")
+      const now = new Date().getTime()
+
+      if (memoizedNews && memoizedTime && now - memoizedTime < 86400000) {
+        setNews(JSON.parse(memoizedNews))
+      } else {
+        try {
+          const response = await fetchNews()
+          console.log("news fetch called")
+          setNews(response.articles.slice(0,5))
+          localStorage.setItem("memoizedNews", JSON.stringify(response.articles.slice(0,5)))
+          localStorage.setItem("memoizedTime", now.toString())
+          console.log(response)
+        } catch (error) {
+          console.log(error.response.data.error)
+        }
+      }
+    }
+    handleFetchNews()
+  }, []) 
+
   return (
-    <div className="info-content">
-      <h4>Trending Topics</h4>
+    <section className="info-content">
+
+      <h4>Trending Topics <TrendUp/></h4>
+      <ul className="news-feed">
+        {news.map(article => {
+          return (
+            <li className="news-article">
+              <a href={article.url}>{article.title.slice(0, article.title.indexOf("-"))}</a>
+              <p>{article.source.name}</p>
+            </li>
+          )
+        })}
+      </ul>
       
-    </div>
+    </section>
   )
 }
