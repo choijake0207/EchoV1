dac73abee4606c91c70f43a18b5569edf6267dc9
choijake0207 <jake.ch0712@gmail.com>
2024-10-14 13:47:15 -0400

    add deleteaccount api call and event handler

--- client/src/Pages/Settings/Settings.jsx
+++ client/src/Pages/Settings/Settings.jsx
@@ -4,6 +4,8 @@ import { updateUserPassword } from '../../Api/PUT'
 import {useDarkMode} from '../../Context/DarkContext'
 import { useAuthorize } from '../../Context/AuthContext'
 import GenericPage from '../../Layouts/GenericPage'
+import { CaretRight } from 'phosphor-react'
+import { deleteAccount } from '../../Api/DELETE'
 import "./settings.css"
 
 export default function Settings() {
@@ -24,6 +26,14 @@ export default function Settings() {
             console.log(error.response.data.error)
         }
     }
+    const handleDeleteAccount = async () => {
+        try {
+            const response = await deleteAccount()
+            console.log(response)
+        } catch (error) {
+            console.log(error.response.data.error)
+        }
+    }
 
   return (
     <GenericPage 
@@ -38,17 +48,17 @@ export default function Settings() {
         {authorizeState.authStatus && (
             <section className="account-settings">
                 <h3>Account</h3>
-                <ul>
-                    <li>
-                        <button type="button" onClick={togglePasswordForm}>Change Password</button>
-                    </li>
-                </ul>
+                <div className="account-options">
+                    <button type="button">Edit Profile <CaretRight/></button>
+                    <button type="button" onClick={togglePasswordForm}>Change Password <CaretRight/></button>
+                    <button type="button" onClic={handleDeleteAccount}>Delete Account <CaretRight/></button>
+                </div>
             </section>
         )}
         <section className="general-settings">
             <h3>General</h3>
-            <ul>
-                <li>
+            <div className="general-options">
+                <div className="dark-mode-container">
                     Dark Mode
                     <label className="switch">
                         <input 
@@ -58,8 +68,8 @@ export default function Settings() {
                         />
                         <span className="slider"></span>
                     </label>
-                </li>
-            </ul>
+                </div>
+            </div>
         </section>
     </GenericPage>
   )
