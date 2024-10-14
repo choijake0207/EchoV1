9d575bf1323c93bb1f853aab19f975e863a78cef
choijake0207 <jake.ch0712@gmail.com>
2024-10-13 22:08:42 -0400

    create friends widget

---
+++ client/src/Components/Sidebar/FriendsTool.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FriendsTool() {
+  return (
+    <div className="friends-tool">
+      <header>
+        <h4>My Friends</h4>
+      </header>
+     
+      
+    </div>
+  )
+}
