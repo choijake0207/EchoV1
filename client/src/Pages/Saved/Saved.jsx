7c608618867e076ca749616a8911992dd657102d
choijake0207 <jake.ch0712@gmail.com>
2024-10-13 21:20:33 -0400

    create page for saved posts

---
+++ client/src/Pages/Saved/Saved.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import GenericPage from "../../Layouts/GenericPage"
+
+export default function Saved() {
+  return (
+    <GenericPage headerTitle="Saved Posts" pageId="saved-page">
+
+    </GenericPage>
+  )
+}
