3bd534a828245e4ccc2a9b70c4349f7740e21a68
choijake0207 <jake.ch0712@gmail.com>
2024-10-14 11:41:04 -0400

    add: useeffect for fetchfriend api / render list from response

--- client/src/Components/Sidebar/FriendsTool.jsx
+++ client/src/Components/Sidebar/FriendsTool.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { fetchFriends } from '../../Api/GET'
+import UserIcon from '../UserIcon/UserIcon'
 
 export default function FriendsTool() {
+  const [friends, setFriends] = useState(null)
+
+  useEffect(() => {
+    const handleFetchFriends = async () => {
+      try {
+        const response = await fetchFriends()
+        setFriends(response)
+        console.log(response)
+      } catch (error) {
+        console.log(error.response.data.error)
+      }
+    }
+    handleFetchFriends()
+  }, [])
+
+
   return (
     <div className="friends-tool">
       <header>
         <h4>My Friends</h4>
       </header>
+      <ul className="friends-list">
+        {friends && 
+          friends.map(friend => {
+            return (
+              <li className="friend-icon" key={friend.id}>
+                <UserIcon username={friend.username}/>
+                <p>{friend.username}</p>
+              </li>
+            )
+          }) 
+        }
+      </ul>
      
       
     </div>
