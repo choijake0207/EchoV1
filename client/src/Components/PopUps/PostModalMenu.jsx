315e4c09e4c12ce680854abe74451be0097720ab
choijake0207 <jake.ch0712@gmail.com>
2024-10-14 10:46:50 -0400

    create modal menu for post component

---
+++ client/src/Components/PopUps/PostModalMenu.jsx
@@ -0,0 +1,26 @@
+import React, { useRef, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
+import { Trash, UserCircle, BookmarkSimple } from 'phosphor-react'
+
+export default function PostModalMenu({onClose, handleDelete, auth, userId}) {
+
+  const modalRef = useRef(null)
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick)
+    return (() => document.removeEventListener("mousedown", handleOutsideClick))
+  },[])
+  const handleOutsideClick = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+          onClose()
+      }
+    }
+  return (
+    <div className="post-modal-menu" ref={modalRef} onClick={onClose}>
+        <NavLink><UserCircle/>View Username's Profile</NavLink>
+        <button><BookmarkSimple/>Save Post</button>
+        {auth.id === userId && 
+          <button onClick={handleDelete}><Trash/>Delete Post</button>
+        }
+    </div>
+  )
+}
