1d2b389e9801eb440c90470fb339436b701cd70a
choijake0207 <jake.ch0712@gmail.com>
2024-10-14 14:38:40 -0400

    add basic components

--- client/src/Pages/Search/Search.jsx
+++ client/src/Pages/Search/Search.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import GenericPage from "../../Layouts/GenericPage"
+import "./search.css"
 
 export default function Search() {
+  const [searchInput, setSearchInput] = useState("")
+
+
+
+
   return (
-    <div className="page" id="search-page">Search</div>
+    <GenericPage headerTitle="Search" pageId="search-page">
+      <section className="search-form">
+        <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        
+      </section>
+      
+      <section className="search-results">
+        <div className="results-filter">
+          <button>Top</button>
+          <button>Latest</button>
+          <button>Accounts</button>
+        </div>
+        
+      </section>
+    </GenericPage>
   )
 }
