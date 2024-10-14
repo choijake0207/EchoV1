c5eaa325eafaf2e4271e3168de07acecde178cc0
choijake0207 <jake.ch0712@gmail.com>
2024-10-14 13:02:51 -0400

    add: useEffect for fetching saved posts, render list

--- client/src/Pages/Saved/Saved.jsx
+++ client/src/Pages/Saved/Saved.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 import GenericPage from "../../Layouts/GenericPage"
+import { fetchSavedPosts } from '../../Api/GET'
+import Post from '../../Components/Post/Post'
+import { useAuthorize } from '../../Context/AuthContext'
 
 export default function Saved() {
+    const [savedPosts, setSavedPosts] = useState([])
+    const {authorizeState} = useAuthorize()
+
+    useEffect(() => {
+        const handleFetchSavedPosts = async () => {
+            try {
+                const response = await fetchSavedPosts()
+                setSavedPosts(response)
+                console.log(response)
+            } catch (error) {
+                console.log(error.response.data.error)
+            }
+        }
+        handleFetchSavedPosts()
+    }, [])
+
+
+
+
+
   return (
     <GenericPage headerTitle="Saved Posts" pageId="saved-page">
+        <ul className="saved-posts">
+            {savedPosts && savedPosts.map(post => {
+                return (
+                    <Post
+                        key={post.id}
+                        id={post.id}
+                        text={post.text}
+                        createdAt={post.createdAt}
+                        userId={post.userId}
+                        username={post.username}
+                        comments={post.Comments}
+                        isSaved={post.SavedPosts.some(obj => obj.userId === authorizeState.id)}
+                        isHomeView={true}
+                    />
+                )
+            })}
+        </ul>
 
     </GenericPage>
   )
