1d95462e4d516a816a67e5f629d395e900a07d0c
choijake0207 <jake.ch0712@gmail.com>
2024-11-02 17:59:34 -0700

    import scroll restorer

--- client/src/Layouts/RootLayout.jsx
+++ client/src/Layouts/RootLayout.jsx
@@ -1,21 +1,23 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect} from 'react'
 import {NavLink, Outlet, useNavigate} from "react-router-dom"
 import { useAuthorize } from '../Context/AuthContext'
 import "./root.css"
 import "./darkMode.css"
-import {House, MagnifyingGlass, User, Gear, Bell, BookmarkSimple, CellSignalFull  } from "phosphor-react"
+import {House, MagnifyingGlass, User, Gear, Bell, BookmarkSimple, CellSignalFull, Sun, Moon, DiamondsFour  } from "phosphor-react"
 import UserIcon from '../Components/UserIcon/UserIcon'
 import InfoCreator from '../Components/Sidebar/InfoCreator'
 import InfoContent from '../Components/Sidebar/InfoContent'
 import InfoLinks from '../Components/Sidebar/InfoLinks'
 import HeaderProfileMenu from '../Components/PopUps/HeaderProfileMenu'
-
+import FriendsTool from '../Components/Sidebar/FriendsTool'
+import { useDarkMode } from '../Context/DarkContext'
+import { ScrollRestoration } from 'react-router-dom'
 export default function RootLayout() { 
 
   const navigate = useNavigate()
   const {authorizeState, logOut}= useAuthorize()
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false)
-
+  const {toggleDarkMode, isDarkMode} = useDarkMode()
 
   const toggleProfileMenu = () => {
     setIsProfileMenuOpen(prev => !prev)
@@ -23,16 +25,17 @@ export default function RootLayout() {
   const handleLogOut = () => {
     logOut()
     navigate("/")
-
   }
+
+
+
  
   return (
     <div className="root-layout">
-
-
+      <ScrollRestoration/>
       <header className="root-header">
-        <h1 className="logo"><CellSignalFull/>echo</h1>
-        {authorizeState.authStatus && (
+        <h1 className="logo"><DiamondsFour/>echo</h1>
+        {authorizeState.authStatus ? (
           <div className="header-username">
             <button onClick={toggleProfileMenu}>
               <UserIcon username={authorizeState.username}/>
@@ -46,7 +49,13 @@ export default function RootLayout() {
               />
             )}
           </div>
-        )}
+        ) : 
+          <>
+            <button className="header-dark-toggle" onClick={toggleDarkMode}>
+              {isDarkMode ? (<><Sun weight="fill"/> Light Mode</>) : (<><Moon weight="fill"/> Dark Mode</>)}
+            </button>
+          </>
+          }
       </header>
 
 
@@ -58,7 +67,7 @@ export default function RootLayout() {
             <>
               <NavLink to="/notifications"><Bell/><span className="nav-text">Notifications</span></NavLink>
               <NavLink to={`/profile/${authorizeState.username}`}><User/><span className="nav-text">Profile</span></NavLink>
-              <NavLink to="/bookmark"><BookmarkSimple/><span className="nav-text">Saved</span></NavLink>
+              <NavLink to="/saved"><BookmarkSimple/><span className="nav-text">Saved</span></NavLink>
             </>
           )}
           <NavLink to="/settings"><Gear/><span className="nav-text">Settings</span></NavLink>
@@ -66,7 +75,10 @@ export default function RootLayout() {
       </aside>
 
       <div className="scrollable-wrapper">
-        <main className="outlet"><Outlet/></main>
+        <main className="outlet">
+          <Outlet/>
+          
+        </main>
 
         <section className="info-sidebar">
           <InfoCreator/>
@@ -77,10 +89,11 @@ export default function RootLayout() {
         
 
       <aside className="tool-sidebar">
+        <FriendsTool
+        
+        />
 
       </aside>
-    
-
 
     </div>
   )
